6f90dab0228d99a6b2b554cd652fe3fb3b6c7b16
JennGoldring <jenngoldring@yahoo.com>
2023-06-10 16:08:33 -0500

    Task 2 half way

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank
+    @Size(max = 50)
+    private String location;
 
+    public Employer() {
+        // no-arg constructor
+    }
+    public String getLocation() {
+        return location;
+    }
 
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import javax.validation.constraints.NotBlank;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @NotBlank
+    private String description;
+
+    public Skill () {
+
+}
+    public String getDescription() {
+        return description;
+}
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
